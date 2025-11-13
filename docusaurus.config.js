768dc3deac9a5069108946a1452b7d6cc2a6ceaf
Aly Andrews <alysa.mcdonald@gmail.com>
2025-11-13 13:52:19 -0500

    Fix broken links: update intro references to about and Skio pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,8 +116,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'About',
+                to: '/docs/about',
               },
             ],
           },
